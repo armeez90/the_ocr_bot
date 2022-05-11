27b025c38a971f1e47bf9f15e33a6274d0922942
armeez90 <105379616+armeez90@users.noreply.github.com>
2022-05-11 21:17:21 +0300

    Update extract_image.py

--- ocrbot/handlers/extract_image.py
+++ ocrbot/handlers/extract_image.py
@@ -13,11 +13,7 @@ def extract_image(update:Update,context:CallbackContext):
     newFile=context.bot.get_file(file_id)
     file_path= newFile.file_path
 
-    keyboard = [[InlineKeyboardButton("English ", callback_data='eng'), InlineKeyboardButton("Russian", callback_data='rus'),InlineKeyboardButton("Czech", callback_data='cze')],
-                [InlineKeyboardButton("Chinese simplified", callback_data='chs'), InlineKeyboardButton("Chinese Traditional", callback_data='cht')],[InlineKeyboardButton("Japanese", callback_data='jpn')] ,
-                [InlineKeyboardButton("Arabic", callback_data='ara'),InlineKeyboardButton("Afrikans", callback_data='AFR'), InlineKeyboardButton("German", callback_data='gre')],
-                [InlineKeyboardButton("Italian", callback_data='ita'),InlineKeyboardButton("Indonesian", callback_data='eng'),InlineKeyboardButton("French", callback_data='fre')],
-                [InlineKeyboardButton ("Spanish", callback_data='spa'),InlineKeyboardButton("Portuguese", callback_data='por'),InlineKeyboardButton("Korean", callback_data='kor')]]
+    keyboard = [[InlineKeyboardButton("English ", callback_data='eng'), InlineKeyboardButton("Russian", callback_data='rus')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     m = update.message.reply_text('Select Language : ', reply_markup=reply_markup,quote=True)
