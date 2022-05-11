31e976757a3075c0152bd2739c15abda645a03d8
armeez90 <105379616+armeez90@users.noreply.github.com>
2022-05-11 21:23:05 +0300

    Update callbackquery_handler.py

--- ocrbot/handlers/callbackquery_handler.py
+++ ocrbot/handlers/callbackquery_handler.py
@@ -16,13 +16,13 @@ def button_click(update:Update,context:CallbackContext):
     query.answer()
     filepath=get_file_path(query.message.chat_id,query.message.message_id)
     if filepath is not None:
-        query.edit_message_text("Extracting text please wait ...")
+        query.edit_message_text("Сканирование текста, подождите ...")
         data=requests.get(f"https://api.ocr.space/parse/imageurl?apikey={API_KEY}&url={filepath}&language={query.data}&detectOrientation=True&filetype=JPG&OCREngine=1&isTable=True&scale=True")
         data=data.json()
         if data['IsErroredOnProcessing']==False:
             message=data['ParsedResults'][0]['ParsedText']
             query.edit_message_text(f"{message}")
         else:
-            query.edit_message_text(text="⚠️Something went wrong, please try again later ⚠️")
+            query.edit_message_text(text="⚠️Что-то пошло не так, попробуйте ещё раз ⚠️")
     else:
-        query.edit_message_text("Something went wrong, Send this image again")
+        query.edit_message_text("Что-то пошло не так, попробуйте ещё раз")
