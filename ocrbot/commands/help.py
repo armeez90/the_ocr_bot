9798013b59b4f1bac74c4e9a4ed5e19616ce57ab
armeez90 <105379616+armeez90@users.noreply.github.com>
2022-05-11 21:24:39 +0300

    Update help.py

--- ocrbot/commands/help.py
+++ ocrbot/commands/help.py
@@ -6,7 +6,7 @@ from ocrbot.helpers.decorators import send_typing_action
 def help(update:Update,context:CallbackContext):
     """Send a message when the command /help is issued."""
     update.message.reply_text(
-        "List of commands available:\
-        \n/start - To start the bot\
-        \n/help - To show this message",quote=True
+        "Доступный список команд:\
+        \n/start - Запустить бота\
+        \n/help - Показать это сообщение",quote=True
     )
